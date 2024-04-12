696afaab73c8bdaa135fb3e1908c223c8ae82058
motarisam <samuelmotari54@gmail.com>
2024-04-12 22:52:35 +0300

    basic

---
+++ basic.py
@@ -0,0 +1,10 @@
+basic_config = example_gen_pb2.Output(
+    split_config=example_gen_pb2.SplitConfig(splits =[
+        example_gen_pb2.SplitConfig.Split(name='train', hash_buckets=4),
+        example_gen_pb2.SplitConfig.Split(name='dev' hash_buckets=1)
+    ]))
+example_gen = tfx.components.CsvExampleGen(
+    instance_name ='Data_Extraction_Spliting',
+    input=external_input(DATA_ROOT),
+    output_config=output_config
+)
